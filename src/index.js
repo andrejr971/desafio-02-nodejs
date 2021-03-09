204a2ee775aaee5819b703351b135e98e097af09
andrejr971 <andre.souza64@fatec.sp.gov.br>
2021-03-09 14:59:13 -0300

    Desafio Completado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const findUser = users.find(user => user.username === username);
+
+  if (!findUser) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = findUser;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length === 10) {
+    return response.status(403).json({ error: 'Update planning for create todo' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id invalidate' });
+  }
+
+  const findUser = users.find(user => user.username === username);
+
+  if (!findUser) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  const findTodo = findUser.todos.find(todo => todo.id === id);
+
+  if (!findTodo) {
+    return response.status(404).json({ error: 'Todo not found the list user' });
+  }
+
+  request.user = findUser;
+  request.todo = findTodo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const findUser = users.find(user => user.id === id);
+
+  if (!findUser) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+
+  request.user = findUser;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
